307077e2bf9ef6f4efebefc1a248e2e1a8ebedf8
Danial <danibazi9@gmail.com>
2022-01-21 16:27:19 +0330

    Complete mock-based class CustomerList

--- src/Customer/CustomersList.java
+++ src/Customer/CustomersList.java
@@ -1,4 +1,17 @@
 package Customer;
 
+import java.util.List;
+
 public class CustomersList {
+    public void addCustomer(String address, float salary, CustomerSpec customerSpec) {
+
+    }
+
+//    public List<Customer> search(CustomerSpec customerSpec) {
+//
+//    }
+
+    public void editCustomer(Customer customer) {
+
+    }
 }

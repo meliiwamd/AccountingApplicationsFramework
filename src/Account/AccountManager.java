f64edea61b3b35141f7070626dc2447555f84eba
ali-f-alfa <ali.farahat78@gmail.com>
2022-01-22 11:58:42 +0330

    complete account manager

--- src/Account/AccountManager.java
+++ src/Account/AccountManager.java
@@ -1,21 +1,32 @@
 package Account;
 
 import Customer.Customer;
+import java.time.*;
+import java.util.*;  
 
 public class AccountManager {
-    public void addAccount(int balance, Customer customer) {
 
+    private static List<Account> Accounts = new ArrayList<Account>();  
+
+    public Account addAccount(Float balance, Customer customer) {
+        Account NewAccount = new Account(balance, LocalDate.now(), customer);
+        Accounts.add(NewAccount);
+        return NewAccount;
     }
 
     public void closeAccount(Account account) {
-
+        Accounts.remove(account);
     }
 
     public void withdrawMoney(Account account, float amount) {
+        if(amount > account.getBalance()){
+            return;
+        }
 
+        account.setBalance(account.getBalance() - amount);
     }
 
     public void depositMoney(Account account, float amount) {
-
+        account.setBalance(account.getBalance() + amount);
     }
 }
